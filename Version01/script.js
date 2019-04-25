c37c42bfbc85252aad614faf7ffc09f4c2a9f64a
dbusii <dtbirkett@gmail.com>
2019-04-25 16:25:35 +0100

    Version 01

--- Version01/script.js
+++ Version01/script.js
@@ -1,11 +1,10 @@
-
-let firstTimer = setInterval(generator, 100);
+let timer = setInterval(generator, 10);
 let makeInput;
 
             function generator(){
                   let makeInput = document.getElementById('character')
                   makeInput.innerHTML = Math.random().toString(36).substring(2,3);
                          if (document.getElementById('character').innerHTML === "a") {
-                        clearInterval(firstTimer);
+                        clearInterval(timer);
                         }
             }
