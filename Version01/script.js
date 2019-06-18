d6723e4995b6490ff4c11273a87913ec90a0d4e0
dbusii <dtbirkett@gmail.com>
2019-06-18 19:38:41 +0100

    Converted to a class

--- Version01/script.js
+++ Version01/script.js
@@ -1,10 +1,23 @@
-let timer = setInterval(generator, 10);
-let makeInput;
 
-            function generator(){
-                  let makeInput = document.getElementById('character')
-                  makeInput.innerHTML = Math.random().toString(36).substring(2,3);
-                         if (document.getElementById('character').innerHTML === "a") {
-                        clearInterval(timer);
-                        }
-            }
+class CharGen{
+	
+	constructor(docElement,assignedChar){
+		this.docElement = docElement
+		this.assignedChar = assignedChar
+		this.timer = setInterval(this.start.bind(this), 10);
+	}
+	
+	start(timerID){
+
+		document.getElementById(`${this.docElement}`).innerHTML = Math.random().toString(36).substring(2,3)
+		 if (document.getElementById(`${this.docElement}`).innerHTML === `${this.assignedChar}`) {
+        clearInterval(this.timer);
+        }
+	}
+
+}
+
+
+let a = new CharGen('character','a')
+let b = new CharGen('character2', 'b')
+let c = new CharGen('character3', 'c')
